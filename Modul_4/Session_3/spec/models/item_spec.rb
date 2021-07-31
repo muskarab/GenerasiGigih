2939ac70c3bcdb43c3b09cb574f3266b4f3cb6ea
muskarab <mustafa.kamal.mk858@gmail.com>
2021-07-31 18:31:55 +0700

    should return true with all atributes

--- Modul_4/Session_3/spec/models/item_spec.rb
+++ Modul_4/Session_3/spec/models/item_spec.rb
@@ -10,25 +10,13 @@ describe Item do
             end
         end
 
-        context 'with no price ' do
-            it 'should return false with nil price' do
-                item = Item.new('id', 'name', nil, 'category')
-                expect(item.valid?).to eq(false)
-            end
-        end
+        # context 'with no price ' do
+        #     it 'should return false with nil price' do
+        #         item = Item.new('id', 'name', nil, 'category')
+        #         expect(item.valid?).to eq(false)
+        #     end
+        # end
     end
 
-    describe '#save' do
-        context 'valid input' do
-            it 'should save data to db' do
-                item = Item.new('id', 'name', 'price', 'category')
-        
-                mock_client = double
-                allow(Mysql2::Client).to receive(:new).and_return(mock_client)
-        
-                expect(mock_client).to receive(:query).with("INSERT INTO items(name, price) VALUES ('#{item.name}', #{item.price})")
-                item.save
-            end
-        end
-    end
+    
 end
