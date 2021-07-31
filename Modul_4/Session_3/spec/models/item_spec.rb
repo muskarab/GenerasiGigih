8f985e32ed8c91ce6137db5235d2963c2ccf080d
muskarab <mustafa.kamal.mk858@gmail.com>
2021-07-31 18:33:44 +0700

    should return false with nil price

--- Modul_4/Session_3/spec/models/item_spec.rb
+++ Modul_4/Session_3/spec/models/item_spec.rb
@@ -16,7 +16,26 @@ describe Item do
                 expect(item.valid?).to eq(false)
             end
         end
+
+        context 'with no name ' do
+            it 'should return false with nil price' do
+                item = Item.new('id', nil, 'price', 'category')
+                expect(item.valid?).to eq(false)
+            end
+        end
     end
 
-    
+    # describe '#save' do
+    #     context 'valid input' do
+    #         it 'should save data to db' do
+    #             item = Item.new('id', 'name', 'price', 'category')
+        
+    #             mock_client = double
+    #             allow(Mysql2::Client).to receive(:new).and_return(mock_client)
+        
+    #             expect(mock_client).to receive(:query).with("INSERT INTO items(name, price) VALUES ('#{item.name}', #{item.price})")
+    #             item.save
+    #         end
+    #     end
+    # end
 end
